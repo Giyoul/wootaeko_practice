928671cba7b72192d40812f919553972384d607a
Giyoul <cla12093@gmail.com>
2026-01-04 13:55:29 +0900

    feat(console): Add print course info feature

--- java-pairmatching-4/src/main/java/pairmatching/view/OutputView.java
+++ java-pairmatching-4/src/main/java/pairmatching/view/OutputView.java
@@ -6,9 +6,24 @@ public class OutputView {
             + "2. 페어 조회\n"
             + "3. 페어 초기화\n"
             + "Q. 종료";
+    private static final String COURSE_INFO = "#############################################\n"
+            + "과정: 백엔드 | 프론트엔드\n"
+            + "미션:\n"
+            + "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n"
+            + "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n"
+            + "  - 레벨3: \n"
+            + "  - 레벨4: 성능개선 | 배포\n"
+            + "  - 레벨5: \n"
+            + "############################################\n"
+            + "과정, 레벨, 미션을 선택하세요.\n"
+            + "ex) 백엔드, 레벨1, 자동차경주";
 
     public void printFunctionSelection() {
         System.out.println(ASK_PAIRMATCHING_CHOICE);
+    }
+
+    public void printCourseInfo() {
+        System.out.println(COURSE_INFO);
     }
 
     public void printExceptionMessage(String message) {
