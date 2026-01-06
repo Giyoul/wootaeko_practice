23d361a38c36b1bf7754e10923e7401c2edeb791
Giyoul <cla12093@gmail.com>
2026-01-06 14:12:07 +0900

    feat(console): add printing initiating message feature

---
+++ java-menu-5/src/main/java/menu/controller/MenuController.java
@@ -0,0 +1,15 @@
+package menu.controller;
+
+import menu.view.OutputView;
+
+public class MenuController {
+    private final OutputView outputView = new OutputView();
+
+    public void run() {
+        getCoachInfo();
+    }
+
+    private void getCoachInfo() {
+        outputView.printInitMessage();
+    }
+}
---
+++ java-menu-5/src/main/java/menu/view/OutputView.java
@@ -0,0 +1,9 @@
+package menu.view;
+
+public class OutputView {
+    private static final String INIT_MESSAGE = "점심 메뉴 추천을 시작합니다.\n";
+
+    public void printInitMessage() {
+        System.out.println(INIT_MESSAGE);
+    }
+}
