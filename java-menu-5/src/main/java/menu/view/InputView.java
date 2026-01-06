05bfbe168d24e998b4aaf30d71caefaffa3f62af
Giyoul <cla12093@gmail.com>
2026-01-06 14:49:46 +0900

    feat(validation): Add checking name size feature

--- java-menu-5/src/main/java/menu/view/InputView.java
+++ java-menu-5/src/main/java/menu/view/InputView.java
@@ -13,12 +13,21 @@ public class InputView {
     public List<String> parseCoachNames(String names) {
         List<String> parsedCoachNames = List.of(names.split(","));
         numberOfCoachNames(parsedCoachNames);
+        checkNameSize(parsedCoachNames);
         return parsedCoachNames;
     }
 
     public void numberOfCoachNames(List<String> coachNames) {
         if (coachNames.size() == 1) {
             throw new IllegalArgumentException("[ERROR] 코치는 최소 2명 이상 입력해야 합니다.");
+        }
+    }
+
+    public void checkNameSize(List<String> coachNames) {
+        for (String name : coachNames) {
+            if (name.length() < 2 || name.length() > 4) {
+                throw new IllegalArgumentException("[ERROR] 코치의 이름은 2글자 이상 4글자 이하여야 합니다.");
+            }
         }
     }
 }
