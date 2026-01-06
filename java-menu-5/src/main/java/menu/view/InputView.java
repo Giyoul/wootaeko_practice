9754b4c06333800269931a01d20856eb3bf850a6
Giyoul <cla12093@gmail.com>
2026-01-06 14:34:45 +0900

    feat(domain): Add coach name parsing feature;

--- java-menu-5/src/main/java/menu/view/InputView.java
+++ java-menu-5/src/main/java/menu/view/InputView.java
@@ -6,8 +6,17 @@ import java.util.List;
 public class InputView {
     public List<String> getCoachName() {
         String names = Console.readLine();
-        List<String> parsedNames = List.of(names.split(","));
-        System.out.println(parsedNames);
+        List<String> parsedNames = parseCoachNames(names);
         return null;
+    }
+
+    public List<String> parseCoachNames(String names) {
+        try {
+            List<String> parsedCoachNames = List.of(names.split(","));
+
+            return parsedCoachNames;
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 구분자는 ,여야 합니다.");
+        }
     }
 }
