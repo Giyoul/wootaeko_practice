12d715c7691c6c545085a65ba68bad2507235fa8
Giyoul <cla12093@gmail.com>
2026-01-06 17:11:15 +0900

    feat(domain): Add selecting category feature

--- java-menu-5/src/main/java/menu/domain/Cuisine.java
+++ java-menu-5/src/main/java/menu/domain/Cuisine.java
@@ -1,15 +1,28 @@
 package menu.domain;
 
-public enum Cuisine {
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    ITALIAN("양식");
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
 
+public enum Cuisine {
+    JAPANESE(1, "일식"),
+    KOREAN(2, "한식"),
+    CHINESE(3, "중식"),
+    ASIAN(4, "아시안"),
+    ITALIAN(5, "양식");
+
+    private final int index;
     private final String cuisineName;
 
-    Cuisine(String cuisineName) {
+    Cuisine(int index, String cuisineName) {
+        this.index = index;
         this.cuisineName = cuisineName;
+    }
+
+    public static Cuisine getRandomCuisine() {
+        int index = Randoms.pickNumberInRange(1, 5);
+        return Arrays.stream(Cuisine.values()).filter(cuisine ->
+                cuisine.index == index)
+                .findFirst()
+                .get();
     }
 }
--- java-menu-5/src/main/java/menu/service/MenuService.java
+++ java-menu-5/src/main/java/menu/service/MenuService.java
@@ -1,7 +1,10 @@
 package menu.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import menu.domain.Coach;
+import menu.domain.Cuisine;
+import menu.domain.Menu;
 
 public class MenuService {
     private List<Coach> coaches;
@@ -12,7 +15,25 @@ public class MenuService {
 
 
     public String recommendMenu() {
+        List<Cuisine> cuisines = new ArrayList<>();
+        List<List<Menu>> recommandMenus = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            cuisines.add(cuisineRecommend(cuisines));
+        }
 
         return "test";
+    }
+
+    private Cuisine cuisineRecommend(List<Cuisine> cuisines) {
+        while (true) {
+            Cuisine recommended = Cuisine.getRandomCuisine();
+            long count = cuisines.stream().filter(cuisine ->
+                            cuisine.equals(recommended))
+                    .count();
+            if (count != 2) {
+                return recommended;
+            }
+        }
     }
 }
